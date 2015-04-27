def6a3bf2b631084e1e8c025628b43a9d4f72724
aodyra <aodyra@gmail.com>
2015-04-27 22:23:41 +0700

    class kotak kasar

---
+++ src/kotakpasir/Kotak.java
@@ -0,0 +1,22 @@
+package kotakpasir;
+
+import java.util.Vector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aodyra
+ */
+public class Kotak {
+    Kotak(){};
+    public int suhu;
+    public Vector<String> history;
+    public Vector<Element> lifeentitas;
+    
+    public void insert()
+}
